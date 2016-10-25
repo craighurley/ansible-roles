ff9125e47362dee9c04aa6c6fdef6bbfddcd5326
Craig Hurley <craig@thehurley.com>
2016-10-25 23:54:18 +1300

    Install additional tools from shell script.

---
+++ test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -ex
+set -o pipefail
+IFS=$'\n'
+
+# # install additional tools
+[[ ! -f "$(which mdl)" ]] && gem install -q --silent mdl
+[[ ! -f "$(which yaml-lint)" ]] && gem install -q --silent yaml-lint
+
+# create a list of mdl rules to ignore
+if [[ ! -f ~/.mdlrc ]] ; then
+    cat << EOF > ~/.mdlrc
+rules "~MD013","~MD014","~MD033"
+EOF
+fi
+
+# find and lint files
+for f in $(find . -type f -not \( -iwholename '*.git*' -o -iwholename '*.tmp*'  \) | sort -u) ; do
+    if file "$f" | grep -i --quiet "\(bash\|shell\) script" ; then
+         shellcheck "$f"
+    elif file "$f" | grep -i --quiet "text" ; then
+        if [[ "$f" = *.md ]]; then
+            mdl "$f"
+        elif [[ "$f" = *.yaml || "$f" = *.yml ]]; then
+            yaml-lint -i -q "$f"
+        fi
+    fi
+done
+
+# create ansible config file
+if [[ ! -f ./ansible.cfg ]] ; then
+    cat << EOF > ./ansible.cfg
+[defaults]
+roles_path=./
+EOF
+fi
+
+# check the syntax of the playbook, including the roles is uses.
+ansible-playbook ./test.yaml -i ./inventory.local --syntax-check
